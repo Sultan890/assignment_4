547469f3c2d2bbb76bdaaa0be82fb6bdb841f529
Zhakulin04 <96425045+Zhakulin04@users.noreply.github.com>
2022-02-19 19:25:34 +0600

    Add files via upload

---
+++ Assignment_4/backend.js
@@ -0,0 +1,25 @@
+function loginn() {
+	console.log(123);
+ //   var username = document.getElementById('uname').value
+	// var password = document.getElementById('psw').value
+
+	// for(var i = 0; i < posts.length; i++) {
+	// 	if(username == posts[i].email && password == posts[i].password) {
+	// 		console.log(username + " is logged in!!!")
+	// 		return
+	// 	}
+	// }
+	// document.write("<p>incorrect username or password</p>")
+ }; 
+ // function register() {
+   
+ // }; 
+ // function search(){
+ // 	var anime = document.getElementById('animesearch');
+ // 	for(var i = 0; i < animes.length; i++) {
+	// 	if(username == animes[i].name) {
+	// 		console.log(username + " is logged in!!!")
+	// 		return
+	// 	}
+	// }
+ // }
